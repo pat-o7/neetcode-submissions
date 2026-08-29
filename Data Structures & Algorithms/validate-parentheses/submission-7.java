1364a5f282e47b080639205b3857b600808b6b68
pat <87922140+pat-o7@users.noreply.github.com>
2026-08-29 00:19:33 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] sChar = s.toCharArray();
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < sChar.length; i++) {
+            if (sChar[i] == '[' || sChar[i] == '(' || sChar[i] == '{') {
+                stack.push(sChar[i]);
+            } else {
+                if (stack.size() > 0 && sChar[i] == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else if (stack.size() > 0 && sChar[i] == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (stack.size() > 0 && sChar[i] == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.size() == 0;
+    }
+}
