33dd103c9ee88e0226e78d71b06aac0a4e0356ba
pat <87922140+pat-o7@users.noreply.github.com>
2026-08-30 00:53:46 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            // if there are still days waiting for warmer days
+                // check if the temperature is warmer
+                while (stack.size() > 0 && temperatures[i] > temperatures[stack.peek()]) {
+                    // add to result
+                    result[stack.peek()] = i - stack.peek();
+                    // remove from waiting list
+                    stack.pop();
+                }
+            // add current day
+            stack.push(i);
+        }
+
+        while (stack.size() > 0) {
+            result[stack.pop()] = 0;
+        }
+        return result;
+    }
+}
