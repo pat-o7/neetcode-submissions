2280428cef908ad5d6fce5595e5f05a4a0e32283
pat <87922140+pat-o7@users.noreply.github.com>
2026-08-30 21:56:17 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = 0;
+        int sell = 1;
+        int profit = 0; // best profit so far
+
+        if (prices.length == 1) {
+            return 0;
+        }
+
+        // while there are days left to sell
+        while (sell < prices.length) {
+            // if the sell is higher than the buy
+            if (prices[buy] < prices[sell]) {
+                profit = Math.max(profit, prices[sell] - prices[buy]);
+                sell++;
+            } else {
+                // if the sell is not higher than the buy
+                buy = sell;
+                sell++;
+            }
+        }
+
+        // 1: buy 7 sell 1, buy 1 sell 5
+        // 2: buy 1 sell 5, profit 4, buy 5 sell 3
+        // 3: buy 5 sell 3, buy 3 sell 6
+
+        return profit;
+    }
+}
