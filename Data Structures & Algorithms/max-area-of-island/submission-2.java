99cecae9e7f5778df174ad4804edd0b3c9914b36
pat <87922140+pat-o7@users.noreply.github.com>
2026-09-16 20:55:33 -0400

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public int maxAreaOfIsland(int[][] grid) {
+
+        int best = 0;
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 1) {
+                    best = Math.max(best, dfs(grid, row, col));
+                }
+            }
+        }
+
+        return best;        
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        if (grid[row][col] == 0) {
+            return 0;
+        }
+
+        grid[row][col] = 0;
+
+        int top = 0;
+        if (row > 0) {
+            top = dfs(grid, row - 1, col);
+        }
+
+        int right = 0;
+        if (col < grid[0].length - 1) {
+            right = dfs(grid, row, col + 1);
+        }
+
+        int bottom = 0;
+        if (row < grid.length - 1) {
+            bottom = dfs(grid, row + 1, col);
+        }
+
+        int left = 0;
+        if (col > 0) {
+            left = dfs(grid, row, col - 1);
+        }
+
+        return 1 + top + right + bottom + left;
+    }
+}
