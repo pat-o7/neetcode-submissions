749bc5f6acaf922c302357204892a7a0325826f7
pat <87922140+pat-o7@users.noreply.github.com>
2026-09-20 03:37:04 -0400

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        // max heap
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> {
+            if (a > b) {
+                return -1;
+            } else if (b > a) {
+                return +1;
+            } else {
+                return 0;
+            }
+        });
+
+        // populate heap
+        for (int i = 0; i < stones.length; i++) {
+            heap.add(stones[i]);
+        }
+
+        // smash rocks
+        while (!heap.isEmpty() && heap.size() > 1) {
+            int rockOne = heap.poll();
+            int rockTwo = heap.poll();
+
+            int rockLeftOver = Math.abs(rockOne - rockTwo);
+
+            if (rockLeftOver > 0) {
+                heap.add(rockLeftOver);
+            }
+        }
+
+        if (heap.isEmpty()) {
+            return 0;
+        }
+        
+        return heap.poll();
+    }
+}
